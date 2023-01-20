a5d7795f35cb5f9bf9ed44c6173eacacc3929ea9
JuanJr <juanj.ortiz@udla.edu.co>
2023-01-19 21:16:59 -0500

    fix STree Lazy

--- Data Structures/Segment Tree Lazy.cpp
+++ Data Structures/Segment Tree Lazy.cpp
@@ -1,7 +1,13 @@
 template<typename T>
 struct STree {
   int n; vector<T> st, lazy;
-  T neutro = T(-1e9);
+  T neutro = T(0);
+
+  STree(int m) {
+    n = m;
+    st.resize(n * 4);
+    lazy.resize(n * 4);
+  }
 
   STree(vector<T> &a) {
     n = sz(a);
@@ -10,44 +16,47 @@ struct STree {
     build(1, 0, n - 1, a);
   }
 
-  T oper(T a, T b) { return max(a,b); }
+  T oper(T a, T b) { return a + b; }
 
   void build(int v, int tl, int tr, vector<T> &a) {
     if(tl == tr) {
       st[v] = a[tl];
       return;
     }
-    int tm = tl + (tr - tl) / 2;
+    int tm = (tl + tr) / 2;
     build(v * 2, tl, tm, a);
     build(v * 2 + 1, tm + 1, tr, a);
     st[v] = oper(st[v * 2], st[v * 2 + 1]);
   }
 
-  void push(int v) {
-    st[v * 2] += lazy[v]; lazy[v * 2] += lazy[v];
-    st[v * 2 + 1] += lazy[v]; lazy[v * 2 + 1] += lazy[v];
-    lazy[v] = 0;
+  void push(int v, int tl, int tr, T &val) {
+    if (!val) return;
+    st[v] += (tr - tl + 1) * val;
+    if (tl != tr) {
+      lazy[v * 2] += val;
+      lazy[v * 2 + 1] += val;
+    }
+    val = 0;
   }
 
   void upd(int v, int tl, int tr, int l, int r, T val) {
-    if(tl > r || tr < l) return;
-    if (l <= tl && tr <= r) {
-      st[v] += val;
-      lazy[v] += val;
+    push(v, tl, tr, lazy[v]);
+    if(tr < l || tl > r) return;
+    if (tl >= l && tr <= r) {
+      push(v, tl, tr, val);
       return;
     }
-    push(v);
-    int tm = tl + (tr - tl) / 2;
+    int tm = (tl + tr) / 2;
     upd(v * 2, tl, tm, l, r, val);
     upd(v * 2 + 1, tm + 1, tr, l, r, val);
     st[v] = oper(st[v * 2], st[v * 2 + 1]);
   }
 
   T query(int v, int tl, int tr, int l, int r) {
+    push(v, tl, tr, lazy[v]);
     if(tl > r || tr < l) return neutro;
     if (l <= tl && tr <= r) return st[v];
-    push(v);
-    int tm = tl + (tr - tl) / 2;
+    int tm = (tl + tr) / 2;
     return oper(query(v * 2, tl, tm, l, r), query(v * 2 + 1, tm + 1, tr, l, r));
   }
 
