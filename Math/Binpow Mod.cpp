8daf42e7e7407bcc878addfaac9a1e6a17dac4d4
Wilmer Emiro Castrillon Calderon <wilmercastrillon98@gmail.com>
2023-01-21 11:15:28 -0500

    Check primes

--- Math/Binpow Mod.cpp
+++ Math/Binpow Mod.cpp
@@ -1,9 +1,19 @@
+ll mulmod(ll a, ll b, ll mod) {
+    ll res = 0;
+    while (b) {
+        if (b & 1) res = (res + a) % mod;
+        a = (a + a) % mod;
+        b >>= 1;
+    }
+    return res;
+}
+
 ll binpow(ll a, ll b, ll m) {
   a %= m;
   ll res = 1;
   while (b) {
-    if (b & 1) res = res * a % m;
-    a = a * a % m;
+    if (b & 1) res = mulmod(res,a,m);
+    a = mulmod(a,a,m);
     b >>= 1;
   }
   return res;
