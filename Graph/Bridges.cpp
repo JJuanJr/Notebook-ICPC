810127b3bf423944530c06aa49dcd1fa14e3510a
JuanJr <juanj.ortiz@udla.edu.co>
2023-02-08 17:07:53 -0500

    Bridges

--- Graph/Bridges.cpp
+++ Graph/Bridges.cpp
@@ -1,36 +1,36 @@
-int n, m;
-vector<vector<int>> ady;
-vector<int> tin, low;
-vector<ii> bridges;
-int timer;
+struct Bridges {
+  int n, timer;
+  vector<bool> vis;
+  vector<int> tin, low;
+  vector<ii> bridges;
 
-void dfs(int v, int p = -1) {
-  tin[v] = low[v] = timer++;
-  for (int &u : ady[v]) {
-    if (p == u) continue;
-    if (tin[u] != -1) {
-      low[v] = min(low[v], low[u]);
-    } else {
-      dfs(u, v);
-      low[v] = min(low[v], low[u]);
-      if (low[u] > tin[v]) {
-        bridges.emplace_back(v, u);
+  Bridges(int m) {
+    n = m;
+    timer = 0;
+    vis.resize(n);
+    tin.assign(n, -1);
+    low.assign(n, -1);
+    forn(i, n)
+      if (!vis[i]) dfs(i);
+  }
+
+  void dfs(int v, int p = -1) {
+    vis[v] = true;
+    tin[v] = low[v] = timer++;
+    for (int &u : g[v]) {
+      if (u == p) continue;
+      if (vis[u]) {
+        low[v] = min(low[v], tin[u]);
+      } else {
+        dfs(u, v);
+        low[v] = min(low[v], low[u]);
+        if (low[u] > tin[v])
+          is_bridge(v, u);
       }
     }
   }
-}
 
-
-cin >> n >> m;
-ady.resize(n);
-while (m--) {
-  int v, u; cin >> v >> u;
-  ady[v].pb(u);
-  ady[u].pb(v);
-}
-
-timer = 0;
-tin.assign(n, -1);
-low.assign(n, -1);
-
-forn (i, n) if (tin[i] == -1) dfs(i);    
+  void is_bridge(int v, int u) {
+    bridges.pb(ii(v, u));
+  }
+};
