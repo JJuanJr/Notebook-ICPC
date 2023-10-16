f6fccd802f14cdc4d7651907e53ba9adf646ee09
SneiderQuintero <dasquirodaron@gmail.com>
2023-10-16 16:38:10 -0500

    update suffix automaton

--- String/Suffix Automata.cpp
+++ String/Suffix Automata.cpp
@@ -1,19 +1,23 @@
 Construye un arbol con los sufijos de una string S.
 Sirve para contar ocurrencias o para verificar que un sufijo pertenezca a S.
 Para verificar el sufijo, haga lo mismo que en match_str.
+
 struct suffixAutomaton {
     struct node {
         int len, link; bool end;
         map<char, int> next;
-        int cnt; ll in, out;
+        int cnt; ll in, out, cntSubstrs;
     };
 
     vector<node> sa;
+     //ocurrencias de estados, usar encontrar kth pequeña lexico all strings
+    vector<ll> cntState; 
     int last; ll substrs = 0;
 
     suffixAutomaton() {}
     suffixAutomaton(string &s) {
-        sa.reserve(s.size()*2);
+        sa.reserve(sz(s)*2);
+        // cntState.reserve(sz(s)*2); 
         last = add_node();
         sa[0].link = -1;
         sa[0].in = 1;
@@ -25,6 +29,7 @@ struct suffixAutomaton {
 
     void add_char(char c) {
         int u = add_node(), p = last;
+        // cntState[u] = 1;
         sa[u].len = sa[last].len + 1;
         while (p != -1 && !sa[p].next.count(c)) {
             sa[p].next[c] = u;
@@ -36,6 +41,7 @@ struct suffixAutomaton {
             int q = sa[p].next[c];
             if (sa[p].len + 1 != sa[q].len) {
                 int clone = add_node();
+                // cntState[clone] = 0;
                 sa[clone] = sa[q];
                 sa[clone].len = sa[p].len + 1;
                 sa[clone].in = 0;
@@ -50,17 +56,9 @@ struct suffixAutomaton {
         }
         last = u;
     }
-
-    void run(string &s) {
-        int u = 0;
-        for (int i = 0; i < s.size(); ++i) {
-            while (u && !sa[u].next.count(s[i])) u = sa[u].link;
-            if (sa[u].next.count(s[i])) u = sa[u].next[s[i]];
-        }
-    }
-
+    //Cuenta la cantidad de ocurrencias de una cadena s
     int match_str(string &s) {
-        int u = 0, n = s.size();
+        int u = 0, n = sz(s);
         for (int i = 0; i < n; ++i) {
             if (!sa[u].next.count(s[i])) return 0;
             u = sa[u].next[s[i]];
@@ -76,13 +74,92 @@ struct suffixAutomaton {
         return sa[u].cnt;
     }
     
-    //La cantidad de caminos, es la cantidad de substring diferentes
+    //Calcular la cantidad de caminos que pertenecen al estado ti, desde ti hasta tn
     ll count_paths(int u) {
-        if (sa[u].out != 0) return sa[u].out;
+        //Out cuenta la cantidad de caminos (cantidad de cadenas distintas)
+        if (sa[u].out != 0) return sa[u].out; //sa[u].cntSubstrs != 0 return sa[u].cntSubstrs
         for (auto &v : sa[u].next)
-            sa[u].out += count_paths(v.ss) + 1;
-        return sa[u].out;
+            sa[u].out += count_paths(v.ss); //sa[u].cntSubstrs += count_paths(v.ss)
+        return ++sa[u].out; //sa[u].cntSubstrs += cntState[u];
     }
     
+    //kth subcadena más pequeña en orden lexicográfico
+    //out para cadenas distintas, cntSubstrs para todas las cadenas llamar antes pre
+    string kth;
+    void dfs_kth(int u, ll &k){ //Antes llamar a count
+        if(k == 0) return; // k < cntState[u] para todas las cadenas
+        k--; // k -= cntState[u];
+        for(auto &v : sa[u].next){
+            if(k < sa[v.ss].out){ //k < sa[v.ss].cntSubstrs
+                kth += v.ff;
+                return dfs_kth(v.ss, k);
+            }
+            k -= sa[v.ss].out; //k -= sa[v.ss],cntSubstrs
+        }
+    }
+    //calcula la cantidad de ocurrencias de los estados
+    void pre(){
+        vector<ii> v(sz(sa));
+        forn(i, sz(sa)) v[i] = {sa[i].len, i};
+        sort(all(v), greater<ii>());
+        for(auto &it : v){
+            int u = it.ss;
+            if(sa[u].link != -1)
+                cntState[sa[u].link] += cntState[u];
+        }
+        cntState[0] = 1;
+    }
+    //longest common substring
+    int lcs(string &t){
+        int n = sz(t);
+        int u = 0, l = 0, best = 0, bestPosition = 0;
+        forn(i, n){
+            while(u && !sa[u].next.count(t[i])){
+                u = sa[u].link;
+                l = sa[u].len;
+            }
+            if(sa[u].next.count(t[i])) u = sa[u].next[t[i]], l++;
+            if(best < l) best = l, bestPosition = i;
+        }
+        return best;
+    }
+    vector<int> LCS, match;
+    void lcsMatch(string &t){
+        match.assign(sz(sa), 0); //usar pivote si toca resetear mucho
+        int u = 0, l = 0;
+        for(int i = 0 ; i < sz(t) ; ++i){
+            while(u && !sa[u].next.count(t[i])){
+                u = sa[u].link;
+                l = sa[u].len;
+            }
+            if(sa[u].next.count(t[i])) u = sa[u].next[t[i]], l++;
+            match[u] = max(match[u], l);
+        }
+        for(int i = sz(sa) - 1 ; i > 0 ; --i)
+            match[i] = max(match[i], match[sa[i].link]);
+        for(int i = 0 ; i < sz(sa) ; ++i)
+            LCS[i] = min(LCS[i], match[i]);
+    }
+
+    //longest common substring de n cadenas
+    int lcs_n(vector<string> &t){
+        const int INF = 1e7;
+        LCS.assign(sz(sa), INF);
+        forn(i, sz(t)) lcsMatch(t[i]);
+        return *max_element(all(LCS));
+    }
+
+    //longitud desde 1 hasta N, return v donde v[i] = num distintas substr de i longitud
+    vector<int> substringDistribution(int lenCadena){
+        vector<int> st(lenCadena + 5);
+        forn(i, sz(sa)){
+            int l = sa[sa[i].link].len + 1; // l minlen subcadena que pertenece al conjunto sa[i]
+            int r = sa[i].len; // r maxlen subcadena que pertenece al conjunto s[i]
+            if(l > 0) st[l]++, st[r + 1]--;
+        }
+        forn(i, lenCadena + 1) st[i + 1] += st[i];
+        return st;
+    }
+
     node& operator[](int i) { return sa[i]; }
 };
